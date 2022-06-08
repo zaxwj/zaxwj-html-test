8a5aa65987107e3ddf62569e85bb9167588919ea
zaxwj <3453728857@qq.com>
2022-06-08 12:40:50 +0800

    First

---
+++ js/payment.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	let showBtn = document.getElementById('showBtn');
+	let detailetBox = document.getElementById('detailetBox');
+	detailetBox.style.display='none';
+	
+	showBtn.onclick = function(){
+		if(detailetBox.style.display=='none'){
+			detailetBox.style.display='block';
+		}else{
+			detailetBox.style.display='none';
+		}
+	}
+}
